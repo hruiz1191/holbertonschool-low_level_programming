a8e26f3179609d0c1d97a7f8dec92fa16549b551
hruiz <elproferuiz@yahoo.com>
2024-10-22 22:24:40 -0400

    great or less

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit on %d is ", n);
+	n = n % 10;
+	if (n > 5)
+		printf("%d and is grater than 5\n", n);
+	if (n == 0)
+		printf("%d is 0\n", n);
+	if ((n < 6) && (n!=0))
+		printf("%d is less than 6 and not 0\n", n);
+	return (0);
+}
