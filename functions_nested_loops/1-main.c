5e837550cc972b8ae1cd95265cd4093241ad3641
hruiz <elproferuiz@yahoo.com>
2024-10-25 12:01:48 -0400

    10 lineas

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * 1-main.c - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0.
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
  * */
-int main(void)
-{
-    print_alphabet();
-    return (0);
+void print_alphabet(void){
+	char letter;
+
+	for ( letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+	_putchar('\n');
 }
--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
- * * main - check the code.
+ * print_alphabet_x10 - print the alphabet
  *
- * Return: Always 0.
- **/
-int main(void)
+ * Return: alphabet in 10 lines.
+ */
+void print_alphabet_x10(void)
 {
-    print_alphabet_x10();
-    return (0);
+	char letter;
+	int line = 0;
+
+	while (line <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+	line++;
+	}
 }
