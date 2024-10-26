e9aaebd6b92acfde8b9920ff3f649143eefda790
hruiz <elproferuiz@yahoo.com>
2024-10-26 12:32:55 -0400

    lower c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - identifica si la letra c es minuscula
+ *
+ * @c: Variable que se va a comparar
+ *
+ * Return: devuelve 1 si es minuscula 0 si es otra letra
+ */
+int _islower(int c)
+{
+	if (c == 99)
+		return (1);
+	return (0);
+}
