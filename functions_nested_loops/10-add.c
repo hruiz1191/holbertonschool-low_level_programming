dd3bc9cc10107812cfea5830335ab0216ac0d769
hruiz <elproferuiz@yahoo.com>
2024-10-27 15:34:42 -0400

    suma 2 digitos

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add- para sumar 2 numeros
+ *
+ * @num1: primer numero a sumar
+ * @num2: segundo numero a sumar
+ *
+ * Return: devuelve la suma de las 2 variables
+ */
+
+int add(int num1, int num2)
+{
+	int suma = num1 + num2;
+
+	return (suma);
+}
