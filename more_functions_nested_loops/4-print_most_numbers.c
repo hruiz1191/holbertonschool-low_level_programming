2e3fce57aec355e5dceb843d9287ab5818e32350
hruiz <elproferuiz@yahoo.com>
2024-10-28 16:19:30 -0400

    numero del 1 al 10 sin 2 ni 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void print_most_numbers(void)
+{
+	int i = 0;
+	for( ;i <= 9; i++)
+	{ 
+		if ((i == 2) || (i == 4))
+			i++;
+				_putchar(i +'0');
+				}
+
+			_putchar('\n');
+}
