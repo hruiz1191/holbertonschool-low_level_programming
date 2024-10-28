447702e292d1a7a1b4845d7dd8c8ce003c6a9403
hruiz <elproferuiz@yahoo.com>
2024-10-28 13:53:28 -0400

    multiplicador

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul- multiplica 2 numeros
+ *
+ * @a: primer numero
+ * @b: segundo numero
+ *
+ * Return: devuelve el resultado de a * b
+ **/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
