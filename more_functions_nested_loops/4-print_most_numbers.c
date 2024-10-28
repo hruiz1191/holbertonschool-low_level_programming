f9c619b9102e9a2253df5848f3f7956c982022a9
hruiz <elproferuiz@yahoo.com>
2024-10-28 19:11:58 -0400

    1 al 9 menos 2 y 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,19 @@
 #include "main.h"
+/**
+ * print_most_numbers - imprime del 0 al 9 menos el 2 y el 4
+ *
+ **/
 
 void print_most_numbers(void)
 {
 	int i = 0;
-	for( ;i <= 9; i++)
-	{ 
+
+	for ( ; i <= 9; i++)
+	{
 		if ((i == 2) || (i == 4))
 			i++;
-				_putchar(i +'0');
-				}
+		_putchar(i + '0');
+	}
 
 			_putchar('\n');
 }
