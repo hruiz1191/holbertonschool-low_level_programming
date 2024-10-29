7ed844826a5987ba154660f64216fabecf552176
hruiz <elproferuiz@yahoo.com>
2024-10-28 21:07:03 -0400

    lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - imprime una linea utilizando _
+ *
+ * @n: indica el largo de lineas
+ */
+
+void print_line(int n)
+{
+	int i;
+
+		if (n == 0)
+			_putchar('\n');
+		else
+			for (i = 0; i <= n; i++)
+			_putchar(95);
+		_putchar('\n');
+}
