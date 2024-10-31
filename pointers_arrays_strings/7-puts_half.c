249c4a60710ccbb77657e06fbf47e260815f5321
hruiz <elproferuiz@yahoo.com>
2024-10-31 16:13:24 -0400

    half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,8 +10,8 @@ void puts_half(char *str)
  {}
 	n = (length_of_the_string - 1) / 2;
 
-for (i = n; i < length_of_the_string; i++)
-	_putchar(str[i+2]);
+for (i = n-1; i < length_of_the_string; i++)
+	_putchar(str[i+1]);
 _putchar('\n');
 
 }
