220340411ae06b3ecb2613e563436288b0ca56e8
hruiz <elproferuiz@yahoo.com>
2024-11-01 07:42:53 -0400

    array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_array - imprime un arreglo 
+ * print_array - imprime un arreglo
  *
- * @a: arreglo 
+ * @a: arreglo
  * @n: numero de elementos en el arreglo
  **/
 void print_array(int *a, int n)
@@ -12,9 +12,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < (n - 1); i++)
 	{
-	printf("%d, ",a[i]);
+	printf("%d, ", a[i]);
 	}
-		if (i == (n-1))
-			printf("%d",a[i]);
+		if (i == (n - 1))
+			printf("%d", a[i]);
 printf("\n");
 }
