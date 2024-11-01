0a43486c367c59f9101175d0739e9373822b71df
hruiz <elproferuiz@yahoo.com>
2024-10-31 20:49:26 -0400

    arrays in pointers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - imprime un arreglo 
+ *
+ * @a: arreglo 
+ * @n: numero de elementos en el arreglo
+ **/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+	printf("%d, ",a[i]);
+	}
+		if (i == (n-1))
+			printf("%d",a[i]);
+printf("\n");
+}
---
+++ pointers_arrays_strings/print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_array - imprime un arreglo 
+ *
+ * @a: arreglo 
+ * @n: numero de elementos en el arreglo
+ **/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+	printf("%d, ",a[i]);
+	}
+		if (i == (n-1))
+			printf("%d",a[i]);
+}
