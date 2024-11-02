eec6ecaff761c67e9ae674f9962817d3729bbf60
hruiz <elproferuiz@yahoo.com>
2024-11-02 18:18:58 -0400

    anade caracteres

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,13 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	i = 0;
-	for (i = 0 ; src[i] != '\0'; i++)
+	for (i = 0 ; dest[i] != '\0'; i++)
 	{}
 
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-		dest[i++] = src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
