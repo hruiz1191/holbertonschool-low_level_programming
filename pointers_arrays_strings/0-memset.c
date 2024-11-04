77e825870fd1d1454187991a036bacd30c61957c
hruiz <elproferuiz@yahoo.com>
2024-11-04 14:26:52 -0400

    memset 1

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * memset - cambia memoria de un puntero por un caracter especifico
+ * memset - funcion
  * @s: puntero a modificar
  * @b: variable a incluir
  * @n: numero de repeticiones deseadas
