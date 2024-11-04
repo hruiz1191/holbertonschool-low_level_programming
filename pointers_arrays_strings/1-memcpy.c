e804b681a48caa5b1220367878f4820d4429bce4
hruiz <elproferuiz@yahoo.com>
2024-11-04 15:22:31 -0400

    copy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - copia de un lugar a otro
+ * @dest: donde se va a copiar
+ * @src: la fuente
+ * @n: cantidad de caracteres a copiar
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+		for (i = 0; i < n; i++)
+		{
+			dest [i] = src [i];
+		}
+	return (dest);
+}
