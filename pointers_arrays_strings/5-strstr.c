34f591584d78ce92e49fbd02ccb52240552d496f
hruiz <elproferuiz@yahoo.com>
2024-11-06 11:03:39 -0400

    haystack

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strstr - Busca caracteres exactos dentro de una linea de caracteres
+ * @haystack: linea de caracteres
+ * @needle: caracter o caracteres deseados
+ * Return: si encuentra caracteres de needle los devuelve.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '0')
+		{
+			one++;
+			two++;
+		}
+		if (*two == '\0')
+			return (haystack);
+	}
+	return (NULL);
+}
