96cd87bbbb9c976d4b2848b8a8f5e6badd472bf7
hruiz <elproferuiz@yahoo.com>
2024-11-06 10:00:37 -0400

    some string

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strpbrk- identifica cuantas veces se repite una letra
+ * @s: caracter entrado
+ * @accept: caracteres deseados
+ **/
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; *(accept + j); j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				break;
+			}
+		}
+		if (*(accept + j) != '\0')
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
+
