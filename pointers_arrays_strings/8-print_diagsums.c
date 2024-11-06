ce9db06f3afc674eaa5d50d72cba9232e73c7c08
hruiz <elproferuiz@yahoo.com>
2024-11-06 15:36:33 -0400

    suma diagonal

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,32 +1,33 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - suma en diagonal
- * @a: puntero con los valores
- * @size: tamano del arreglo
- **/
-
- void print_diagsums(int *a, int size)
+ * print_diagsums - suma de 2 diagonales.
+ * @a: source
+ * @size: tamano de la matriz
+ *
+ * Return: suma diagnal.
+ */
+void print_diagsums(int *a, int size)
 {
 	int i;
 	int j;
-	int sum1 = 0;
-	int sum2 = 0;
+	int sum1;
+	int sum2;
+	int step1;
+	int step2;
 
-	for (i = 0; i <= size; i++)
+	sum1 = 0;
+	sum2 = 0;
+	for (i = 0; i <= (size - 1); i++)
 	{
-		for (j = 0; j <= size; j++)
-		{
-			if(i == j)
-			{
-				sum1 += a[i * size + j];
-			}
-			if(i + j == size - 1)
-			{
-				sum2 += a[i * size + j];
-			}
-		}
+		step1 =  (size + 1) * i;
+		sum1 = sum1 + *(a + step1);
+	}
+
+	for (j = 1; j <= size; j++)
+	{
+		step2 = (size - 1) * j;
+		sum2 = sum2 + *(a + step2);
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
-
