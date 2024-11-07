784766f2032cc303bf24e10af63b060615c4ae35
hruiz <elproferuiz@yahoo.com>
2024-11-07 14:03:17 -0400

    strlen recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Imprime un string
+ * @s: caracteres a imprimir
+ * Return: numero de caracteres
+ **/
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return(1 + _strlen_recursion(s + 1));
+	}
+	else
+		return (0);
+}
