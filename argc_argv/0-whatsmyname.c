cb290aceb384dab304f1fcf93b8be1a81a7ffc78
hruiz <elproferuiz@yahoo.com>
2024-11-09 15:49:23 -0400

    whats my name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - funcion principal
+ * @argc: contador
+ * @argv: posicion
+ * Return: Devuelve 0
+ **/
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s", argv[argc]);
+	return (0);
+}
