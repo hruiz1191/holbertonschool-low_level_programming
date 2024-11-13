d9bcd7a2f3c062833f1a10db910ef5ede7e89efe
hruiz <elproferuiz@yahoo.com>
2024-11-13 12:48:51 -0400

    malloc y exit

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
- /**
-  * malloc_checked - verifica si se aloja la memoria
-  * @b: cantdad de memoria
-  * Return: devuelve 98
-  **/
+/**
+ * malloc_checked - verifica si se aloja la memoria
+ * @b: cantdad de memoria
+ * Return: devuelve 98
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
