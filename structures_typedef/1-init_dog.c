1025613c00cc34c75066db6a31b9a860897b5649
hruiz <elproferuiz@yahoo.com>
2024-11-14 14:58:42 -0400

    ini struct

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - inicializa la estructura dog
+ * @name: nombre del perro
+ * @age: edad del perro
+ * @owner: dueno del perro
+ **/
+
+void init_dog(struct dog *d, char *name, float age, char owner)
+{
+d -> name;
+d -> age;
+d -> owner;
+}
+
