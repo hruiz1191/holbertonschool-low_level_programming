57f566c46fddcca0f0d218518ac07bd182d11436
hruiz <elproferuiz@yahoo.com>
2024-11-16 00:01:13 -0400

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,77 +1,38 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
- * _strlen - returns the lenght of a string
- * @s: pointer to s
- *
- * Return: 0 on success
- *
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s)
-	{
-		len++;
-		s++;
-	}
-	return (len);
-}
-/**
- * _strdub  - duplica el string
- * @str: pointer to duplicar
- *
- * Return: the pointer to str
- *
- */
-char *_strdub(char *str)
-{
-	char *res = NULL;
-	int i = 0;
-
-	if (str != NULL)
-	{
-		res = malloc(_strlen(str) + 1);
-		if (res != NULL)
-		{
-		for (i = 0; i <= _strlen(str); i++)
-			res[i] = str[i];
-		}
-	}
-	return (res);
-}
-
-/**
- * new_dog - creates a new dog
- * @name: name
- * @age: age
- * @owner: owner
- * Return: pointer to new struct
- */
+  * new_dog - creates a new struct of type dog
+  * @name: struct parameter name
+  * @age: struct parameter age
+  * @owner: struct parameter owner
+  * Return: returns pointer to buffer of datatype dog
+  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_d = NULL;
+	int nlen, olen, i;
+	dog_t *doggy;
 
-	new_d = malloc(sizeof(dog_t));
-	if (new_d == NULL)
-	{
-		new_d->name = _strdub(name);
-	if (new_d->name == NULL)
-	{
-		free(new_d);
+	nlen = olen = 0;
+	while (name[nlen++])
+		;
+	while (owner[olen++])
+		;
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
 		return (NULL);
-	}
 
-	new_d->owner = _strdub(owner);
-	if (new_d->owner == NULL)
-	{
-		free(new_d->name);
-		free(new_d);
+	doggy->name = malloc(nlen * sizeof(doggy->name));
+	if (doggy == NULL)
 		return (NULL);
-	}
-	new_d->age = age;
-	}
-	return (new_d);
+	for (i = 0; i < nlen; i++)
+		doggy->name[i] = name[i];
+
+	doggy->age = age;
+
+	doggy->owner = malloc(olen * sizeof(doggy->owner));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < olen; i++)
+		doggy->owner[i] = owner[i];
+	return (doggy);
 }
