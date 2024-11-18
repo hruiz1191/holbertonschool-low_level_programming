54b9962b8d3c55405a4425dde6c1927ae15c07de
hruiz <elproferuiz@yahoo.com>
2024-11-18 14:20:47 -0400

    write name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+
 /**
  * print_name - imprime el nombre
  * @name: nombre a imprimir
  * @f: nombre de function pointer
  */
 
-void print_name (char *name, void(*f)(char *))
+void print_name(char *name, void(*f)(char *))
 {
-
-f(name);
-
+	f(name);
 }
 
