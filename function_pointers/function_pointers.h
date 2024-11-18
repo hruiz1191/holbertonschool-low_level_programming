81ec48eea7be5f305ea2bc9c930a19edc06f1c77
hruiz <elproferuiz@yahoo.com>
2024-11-18 14:48:18 -0400

    write name

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,3 +1,11 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+
+#include <stdio.h>
+#include <stddef.h>
+
 int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
