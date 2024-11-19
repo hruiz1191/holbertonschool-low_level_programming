fda8b6531bcadcaa68883e8973f4ea3d2385e5ba
hruiz <elproferuiz@yahoo.com>
2024-11-19 14:15:42 -0400

    int index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -4,6 +4,7 @@
  * @array: arreglo para explorar
  * @size: tamano del arreglo
  * @cmp: comparar
+ * Return: devuelve  valor o -1
  **/
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -13,12 +14,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	if (array && cmp)
 	{
-		for(i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			if ((cmp)(array[i]))
-			return(i);
+			return (i);
 		}
-		return(-1);
+		return (-1);
 	}
-	return(-1);
- }
+	return (-1);
+}
