cfa3d37e5c6eaf159524e82fc0f1f1107d96689f
hruiz <elproferuiz@yahoo.com>
2024-11-24 15:15:06 -0400

    Imprime cadena de strings

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,5 +3,6 @@
 void _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *sepatrator,const unsigned int n, ...);
 
 #endif
